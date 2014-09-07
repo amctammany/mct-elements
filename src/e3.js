c054c3454c632dc0f79601690476b74f95fb5fae
Alex McTammany <apmctammany@gmail.com>
2014-09-07 02:02:39 -0400

    redid everything and it works

--- src/e3.js
+++ src/e3.js
@@ -3,6 +3,11 @@ var raf = require('./raf');
 var canvas = document.querySelector('#game');
 var ctx = canvas.getContext('2d');
 
+var $up = document.getElementById('direction-up');
+var $right = document.getElementById('direction-right');
+var $down = document.getElementById('direction-down');
+var $left = document.getElementById('direction-left');
+
 var _dirty = true; // Dirty Checking for Animation Loop
 var _cube, _currentFace = 0; // Loaded Cube and displayed face
 var OPPOSITE_FACES = {
@@ -12,6 +17,14 @@ var OPPOSITE_FACES = {
   3: 1,
   4: 2,
   5: 0,
+};
+var ADJACENT_FACES = {
+  0: [3,2,1,4],
+  1: [0,2,5,4],
+  2: [0,3,5,1],
+  3: [0,4,5,2],
+  4: [0,1,5,3],
+  5: [1,2,3,4]
 };
 
 var BODY_TYPES = {
@@ -63,6 +76,14 @@ var ELEMENT_FILL = {
 };
 
 // }
+// Helper Functions {
+var getRow = function (index, size) {
+  return Math.floor(index/size);
+};
+var getColumn = function (index, size) {
+  return index % size;
+};
+// }
 // Cube {
 var Cube = function (config) {
   this.size = config.size;
@@ -72,7 +93,9 @@ var Cube = function (config) {
   this.faces = new Array(6);
   for (var i = 0; i < 6; i++) {
     this.faces[i] = new Face(i, this.size);
+    this.faces[i].cube = this;
   }
+  this.currentFace = this.faces[0];
   this.bodies = this.loadBodies(config.bodies);
 };
 Cube.prototype = {
@@ -81,6 +104,16 @@ Cube.prototype = {
     return bodies.map(function (body) {
       return new Body(cube, body);
     });
+  },
+  shiftDirection: function (direction) {
+    var origin = (direction + 2) % 4;
+    var face = this.getFace(this.currentFace.getNeighbors()[direction]);
+    face.setOrientation(this.currentFace.id, origin);
+    this.currentFace = face;
+
+  },
+  getFace: function (id) {
+    return this.faces[id];
   },
   addStream: function (face, index, element, direction) {
     this.streams.push(new Stream(face, index, element, direction));
@@ -107,8 +140,9 @@ Cube.prototype = {
   checkVictoryConditions: function () {
 
   },
-  drawFace: function (ctx, f, orientation) {
-    var face = this.faces[f];
+  drawFace: function (ctx) {
+    var face = this.currentFace;
+    var orientation = face.orientation;
     var dL = this.dL;
     for (var row = 0; row < this.size; row++) {
       for (var column = 0; column < this.size; column++) {
@@ -136,6 +170,10 @@ Cube.prototype = {
       }
     }
 
+    ctx.fillStyle = 'white';
+    ctx.font = '60px Verdana';
+    ctx.fillText(this.currentFace.id + ' - ' + this.currentFace.orientation, 20, 50);
+
   }
 
 };
@@ -145,6 +183,8 @@ var Face = function (id, size) {
   this.id = id;
   this.size = size;
   this.cells = {};
+  this.orientation = 0;
+  this.neighbors = ADJACENT_FACES[id];
 };
 
 Face.prototype = {
@@ -152,18 +192,37 @@ Face.prototype = {
     var index = this.getIndex(row, column, orientation);
     return this.cells[index];
   },
+  getNeighbors: function () {
+    //return this.neighbors;
+    var orientation = this.orientation;
+    var head = this.neighbors.slice(0, orientation);
+    var tail = this.neighbors.slice(orientation);
+    return tail.concat(head);
+  },
+  setOrientation: function (face, origin) {
+    var i = this.neighbors.indexOf(face);
+    if (i < 0) {
+      console.warn('What the fucK!');
+    }
+    var dI = i - origin;
+    this.orientation = (4 + dI) % 4;
+
+  },
   getIndex: function (row, column, orientation) {
-    var size = this.size;
+    var size = this.size - 1;
     if (orientation === undefined || orientation === 0) {
-      return (row * size) + column;
-    } else if (orientation === 0) {
+      return (row * this.size) + column;
+    } else if (orientation === 1) {
+      return this.getIndex(column, size - row);
 
     } else if (orientation === 2) {
+      return this.getIndex(size - row, size - column);
 
     } else if (orientation === 3) {
+      return this.getIndex(size - column, row);
 
     } else {
-      console.error('Invalid orientation');
+      console.error('Invalid orientation: ' + orientation);
     }
   },
   shiftIndex: function (index, direction) {
@@ -175,8 +234,8 @@ Face.prototype = {
 
   },
   checkBoundaries: function (index, direction) {
-    var row = Math.floor(index / this.size);
-    var column = index % this.size;
+    var row = getRow(index, this.size);
+    var column = getColumn(index, this.size);
     var border = -1;
     var size = this.size;
     switch (direction) {
@@ -197,6 +256,52 @@ Face.prototype = {
         break;
     }
     return border;
+  },
+  getEdgePosition: function (index, border) {
+    var row = getRow(index, this.size);
+    var column = getColumn(index, this.size);
+    var position = (border % 2) === 0 ? column : row;
+    return position;
+  },
+  checkBorders: function (stream) {
+    var index = stream.current;
+    var border = this.checkBoundaries(index, stream.direction);
+    if (border > -1) {
+      var element = stream.element;
+      var face = this.cube.getFace(this.getNeighbors()[border]);
+      var position = this.getEdgePosition(index, border);
+      face.transferStream(element, this.id, position);
+      return 'foobar';
+
+    } else {
+      return false;
+    }
+  },
+  transferStream: function (element, face, position) {
+    var row, column;
+    var border = this.neighbors.indexOf(face);
+    switch (border) {
+      case 0:
+        row = 0;
+        column = position;
+        break;
+      case 1:
+        row = position;
+        column = this.size - 1;
+        break;
+      case 2:
+        row = this.size - 1;
+        column = position;
+        break;
+      case 3:
+        row = position;
+        column = 0;
+        break;
+    }
+    var index = this.getIndex(row, column, 0)
+    this.cells[index] = 'foo';
+    console.log('transfer stream at ' + index);
+    return this.getIndex(row, column);
 
   },
 };
@@ -229,8 +334,11 @@ Body.prototype = {
       case 2: // Collector
         break;
     }
-  }
-}
+  },
+  collidesWith: function (stream) {
+    console.log('stream body collision');
+  },
+};
 // }
 // Stream {
 var Stream = function (face, index, element, direction) {
@@ -254,14 +362,24 @@ Stream.prototype = {
     } else {
       this.face.cells[this.current] = this;
     }
-    var border = this.face.checkBoundaries(this.current, this.direction);
-    if (border > -1) {
+    if (this.face.checkBorders(this)) {
       this.completed = true;
+    } else {
+      this.current = this.face.shiftIndex(this.current, this.direction);
     }
-    this.current = this.face.shiftIndex(this.current, this.direction);
     //this.completed = true;
 
   },
+  collidesWith: function (stream, index) {
+    console.log('stream collision');
+  },
+};
+// }
+// Edge Transfer {
+var Transfer = function (stream, border) {
+  this.face = _cube.getFace(stream.face.getNeighbors()[border]);
+  this.face.cells[4] = stream;
+
 }
 // }
 // Level Definitions {
@@ -275,16 +393,55 @@ var levels = [
       [0, 3, 0, 64, 3],
     ],
   },
+  {
+    size: 5,
+    bodies: [
+      [0, 0, 1, 12, 0],
+      [0, 0, 2, 12, 0],
+      [0, 0, 3, 12, 0],
+      [0, 0, 4, 12, 0],
+    ]
+  }
 ];
 // }
 // Animation Loop {
+_cube = new Cube(levels[1]);
 raf.start(function (elapsed) {
   if (_dirty) {
     _cube.initStreams();
     _dirty = false;
   }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  _cube.drawFace(ctx, _currentFace)
-})
+  _cube.drawFace(ctx);
+});
 // }
-_cube = new Cube(levels[0]);
+// User Input {
+$up.onclick = function () {
+  _cube.shiftDirection(0);
+  resetUI();
+  //_dirty = true;
+};
+$right.onclick = function () {
+  _cube.shiftDirection(1);
+  resetUI();
+  //_dirty = true;
+};
+$down.onclick = function () {
+  _cube.shiftDirection(2);
+  resetUI();
+  //_dirty = true;
+};
+$left.onclick = function () {
+  _cube.shiftDirection(3);
+  resetUI();
+  //_dirty = true;
+};
+function resetUI () {
+  var neighbors = _cube.currentFace.getNeighbors();
+  $up.innerHTML = neighbors[0];
+  $right.innerHTML = neighbors[1];
+  $down.innerHTML = neighbors[2];
+  $left.innerHTML = neighbors[3];
+}
+resetUI();
+// }
